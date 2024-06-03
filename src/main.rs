156df20b530251802b528122f5c76963e056908c
Alex Rodriguez <alxrodav@gmail.com>
2024-06-03 11:11:02 +0200

    chore: axum setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:4000")
+        .await
+        .unwrap();
+
+    println!("Lintening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> Html<String> {
+    Html("<h1>Hello World!!!</h1>".into())
 }
